f233fd637e055f2420492c5e945c3f9b16f893a0
Thomas Marchand <thomas.marchand@tuta.io>
2023-02-02 13:23:11 +0200

    feat: extract button

---
+++ src/components/connection/connect.tsx
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from "react";
+import { useConnectors, useAccount, useStarknet } from "@starknet-react/core";
+import { useDisplayName } from '@/hooks/displayname';
+import WalletSelector from '@/components/connection/walletSelector';
+import styles from '@/styles/components/connection/Connect.module.css'
+
+export default function Connect() {
+
+    // connection
+    const [showWalletSelector, setShowWalletSelector] = useState<boolean>(false);
+    const [isWrongNetwork, setIsWrongNetwork] = useState(false);
+
+    // starknet-react
+    const { address } = useAccount();
+    const { available, connect, disconnect } = useConnectors();
+    const displayname = useDisplayName(address);
+    const { library } = useStarknet();
+
+    useEffect(() => {
+        if (!address) return;
+
+        const STARKNET_NETWORK = {
+            mainnet: "0x534e5f4d41494e",
+            testnet: "0x534e5f474f45524c49",
+        };
+
+        if (library.chainId === STARKNET_NETWORK.testnet && process.env.NEXT_PUBLIC_NETWORK === "mainnet") {
+            setIsWrongNetwork(true);
+        } else if (
+            library.chainId === STARKNET_NETWORK.mainnet &&
+            process.env.network === "testnet"
+        ) {
+            setIsWrongNetwork(true);
+        } else {
+            setIsWrongNetwork(false);
+        }
+    }, [library, address]);
+
+    function disconnectByClick(): void {
+        disconnect();
+        setIsWrongNetwork(false);
+    }
+
+    return (
+        <>
+            <button className={styles.button}
+                onClick={
+                    address
+                        ? () => disconnectByClick()
+                        : available.length === 1
+                            ? () => connect(available[0])
+                            : () => setShowWalletSelector(true)}>
+                {address ? displayname : "connect"}
+            </button>
+            {isWrongNetwork
+                ? <div>wrong network</div>
+                : showWalletSelector
+                    ? <WalletSelector closeWalletSelector={() => setShowWalletSelector(false)} />
+                    : <></>}
+        </>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,45 +1,8 @@
 import Head from 'next/head'
 import styles from '@/styles/Home.module.css'
-import React, { useState, useEffect } from "react";
-import { useConnectors, useAccount, useStarknet } from "@starknet-react/core";
-import { useDisplayName } from '@/hooks/displayname';
-import WalletSelector from '@/components/connection/walletSelector';
+import React from "react";
+import Connect from '@/components/connection/connect';
 export default function Home() {
-
-  // connection
-  const [showWalletSelector, setShowWalletSelector] = useState<boolean>(false);
-  const [isWrongNetwork, setIsWrongNetwork] = useState(false);
-
-  // starknet-react
-  const { address } = useAccount();
-  const { available, connect, disconnect } = useConnectors();
-  const displayname = useDisplayName(address);
-  const { library } = useStarknet();
-
-  useEffect(() => {
-    if (!address) return;
-
-    const STARKNET_NETWORK = {
-      mainnet: "0x534e5f4d41494e",
-      testnet: "0x534e5f474f45524c49",
-    };
-
-    if (library.chainId === STARKNET_NETWORK.testnet && process.env.NEXT_PUBLIC_NETWORK === "mainnet") {
-      setIsWrongNetwork(true);
-    } else if (
-      library.chainId === STARKNET_NETWORK.mainnet &&
-      process.env.network === "testnet"
-    ) {
-      setIsWrongNetwork(true);
-    } else {
-      setIsWrongNetwork(false);
-    }
-  }, [library, address]);
-
-  function disconnectByClick(): void {
-    disconnect();
-    setIsWrongNetwork(false);
-  }
 
   return (
     <>
@@ -51,20 +14,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <button
-          onClick={
-            address
-              ? () => disconnectByClick()
-              : available.length === 1
-                ? () => connect(available[0])
-                : () => setShowWalletSelector(true)}>
-          {address ? displayname : "connect"}
-        </button>
-        {isWrongNetwork
-          ? <div>wrong network</div>
-          : showWalletSelector
-            ? <WalletSelector closeWalletSelector={() => setShowWalletSelector(false)} />
-            : <></>}
+        <Connect />
       </main>
     </>
   )
