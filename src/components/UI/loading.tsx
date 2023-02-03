440c5b0cb9b4726cda462d3492dd9015f817f0e4
Nico <60229704+nicolasito1411@users.noreply.github.com>
2023-02-03 17:30:55 +0100

    Adding a loading screen when deploying and notifications

--- src/components/UI/loading.tsx
+++ src/components/UI/loading.tsx
@@ -1,13 +1,18 @@
 import React, { FunctionComponent } from "react";
 import { MutatingDots } from "react-loader-spinner";
 
-const Loading:FunctionComponent = () => {
+interface LoadingProps {
+    [key: string]: any;
+}
+
+const Loading:FunctionComponent<LoadingProps> = (props) => {
     return <MutatingDots
         height="100"
         width="100"
         color="#19AA6E"
         secondaryColor="#BF9E7B"
         ariaLabel="loading"
+        {...props}
     />
 };
 
