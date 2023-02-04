5f390d5c6a80ea5ff5c198c230984ca25205d1bc
Nico <60229704+nicolasito1411@users.noreply.github.com>
2023-02-04 14:08:29 +0100

    Adding the wrong network warning

--- src/components/connection/connect.tsx
+++ src/components/connection/connect.tsx
@@ -6,40 +6,17 @@ import styles from '@/styles/components/connection/Connect.module.css'
 import Button from "../UI/button";
 
 export default function Connect() {
-
     // connection
     const [showWalletSelector, setShowWalletSelector] = useState<boolean>(false);
-    const [isWrongNetwork, setIsWrongNetwork] = useState(false);
 
     // starknet-react
     const { address } = useAccount();
     const { available, connect, disconnect } = useConnectors();
     const displayname = useDisplayName(address);
-    const { library } = useStarknet();
 
-    useEffect(() => {
-        if (!address) return;
-
-        const STARKNET_NETWORK = {
-            mainnet: "0x534e5f4d41494e",
-            testnet: "0x534e5f474f45524c49",
-        };
-
-        if (library.chainId === STARKNET_NETWORK.testnet && process.env.NEXT_PUBLIC_NETWORK === "mainnet") {
-            setIsWrongNetwork(true);
-        } else if (
-            library.chainId === STARKNET_NETWORK.mainnet &&
-            process.env.network === "testnet"
-        ) {
-            setIsWrongNetwork(true);
-        } else {
-            setIsWrongNetwork(false);
-        }
-    }, [library, address]);
 
     function disconnectByClick(): void {
         disconnect();
-        setIsWrongNetwork(false);
     }
 
     return (
@@ -54,9 +31,7 @@ export default function Connect() {
                 {address ? displayname : "connect"}
                 </Button>
             </div>
-            {isWrongNetwork
-                ? <div>wrong network</div>
-                : showWalletSelector
+            {showWalletSelector
                     ? <WalletSelector closeWalletSelector={() => setShowWalletSelector(false)} />
                     : <></>}
         </>
---
+++ src/components/connection/networkSelector.tsx
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from "react";
+import { useConnectors, useAccount, useStarknet } from "@starknet-react/core";
+import Button from "../UI/button";
+import ModalMessage from "../UI/modalMessage";
+import styles from '@/styles/components/connection/networkSelector.module.css'
+
+export default function NetworkSelector() {
+    // connection
+    const [isWrongNetwork, setIsWrongNetwork] = useState(false);
+
+    // starknet-react
+    const { address } = useAccount();
+    const { disconnect } = useConnectors();
+    const { library } = useStarknet();
+
+    useEffect(() => {
+        if (!address) return;
+
+        const STARKNET_NETWORK = {
+            mainnet: "0x534e5f4d41494e",
+            testnet: "0x534e5f474f45524c49",
+        };
+        if (library.chainId === STARKNET_NETWORK.testnet && process.env.NEXT_PUBLIC_NETWORK === "mainnet") {
+            setIsWrongNetwork(true);
+        } else if (
+            library.chainId === STARKNET_NETWORK.mainnet &&
+            process.env.network === "testnet"
+        ) {
+            setIsWrongNetwork(true);
+        } else {
+            setIsWrongNetwork(false);
+        }
+    }, [library, address]);
+
+    function disconnectByClick(): void {
+        disconnect();
+        setIsWrongNetwork(false);
+    }
+
+    return (
+        isWrongNetwork
+            ? <ModalMessage
+            open={true}
+            title={"Wrong network"}
+            closeModal={() => setIsWrongNetwork(false)}
+            message={
+                <div className={styles.container}>
+                    <p>
+                        This app only supports Starknet {process.env.NEXT_PUBLIC_NETWORK}, you have to change your
+                        network to be able use it.
+                    </p>
+                    <div className={styles.disconnectButton}>
+                        <Button onClick={() => disconnectByClick()}>
+                            Disconnect
+                        </Button>
+                    </div>
+                </div>
+            }
+            />
+            : null
+    )
+}
