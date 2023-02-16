0c2ebf229e360edd1123c3b2da30d42a41c148c8
Iris <iris.devillars@gmail.com>
2023-02-16 14:28:11 +0100

    feat: add sbt minting page

---
+++ src/components/selectIdentity.tsx
@@ -0,0 +1,95 @@
+import {
+  FormControl,
+  FormHelperText,
+  InputLabel,
+  ListItemIcon,
+  ListItemText,
+  MenuItem,
+  Select,
+} from "@mui/material";
+import { useAccount } from "@starknet-react/core";
+import React, { FunctionComponent, useEffect, useState } from "react";
+import { hexToFelt } from "../../utils/felt";
+
+type SelectIdentityProps = {
+  tokenId: string;
+  changeTokenId: (value: string) => void;
+  defaultText?: string;
+};
+
+type Identity = {
+  id: string;
+  domain?: string;
+};
+
+const SelectIdentity: FunctionComponent<SelectIdentityProps> = ({
+  tokenId,
+  changeTokenId,
+  defaultText = "Mint a new starknet.id",
+}) => {
+  const { account } = useAccount();
+  const [ownedIdentities, setOwnedIdentities] = useState<Identity[] | []>([]);
+
+  useEffect(() => {
+    if (account) {
+      fetch(
+        `https://app.starknet.id/api/indexer/addr_to_full_ids?addr=${hexToFelt(
+          account.address
+        )}`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          setOwnedIdentities(data.full_ids);
+        });
+    }
+  }, [account]);
+
+  return (
+    <div className="mt-3">
+      <FormControl fullWidth>
+        <InputLabel>Starknet.id</InputLabel>
+        <Select
+          value={tokenId}
+          defaultValue={ownedIdentities[0]?.id}
+          label="Starknet.id"
+          onChange={(e) => changeTokenId(e.target.value)}
+          sx={{
+            "& .MuiSelect-select": {
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            },
+          }}
+        >
+          <MenuItem value={0}>
+            <ListItemIcon>
+              <img
+                width={"25px"}
+                src="/visuals/StarknetIdLogo.svg"
+                alt="starknet.id avatar"
+              />
+            </ListItemIcon>
+            <ListItemText primary={defaultText} />
+          </MenuItem>
+          {ownedIdentities.map((identity: Identity, index: number) => (
+            <MenuItem key={index} value={identity.id}>
+              <ListItemIcon>
+                <img
+                  width={"25px"}
+                  src={`https://www.starknet.id/api/identicons/${identity.id}`}
+                  alt="starknet.id avatar"
+                />
+              </ListItemIcon>
+              <ListItemText primary={identity?.domain ?? identity.id} />
+            </MenuItem>
+          ))}
+        </Select>
+        <FormHelperText>
+          Choose the starknet identity you want to link with your identity token
+        </FormHelperText>
+      </FormControl>
+    </div>
+  );
+};
+
+export default SelectIdentity;
---
+++ src/pages/[SbtAddress].tsx
@@ -0,0 +1,240 @@
+import {
+  useAccount,
+  useContract,
+  useStarknet,
+  useStarknetCall,
+  useStarknetExecute,
+} from "@starknet-react/core";
+import { NextPage } from "next";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import BN from "bn.js";
+import { ec, hash } from "starknet";
+import Button from "@/components/UI/button";
+import SelectIdentity from "@/components/selectIdentity";
+import Connect from "@/components/connection/connect";
+import styles from "../styles/SbtGenerator.module.css";
+import sbt_abi from "@/abi/starknet/sbt_abi.json";
+import { TextField } from "@mui/material";
+import SbtNetworkSelector from "@/components/connection/sbtNetworkSelector";
+
+export type MetadataProps = {
+  name: string;
+  desc: string;
+  image: string;
+};
+
+export type CallDataProps = {
+  calldata: (string | number)[];
+  contractAddress: string;
+  entrypoint: string;
+};
+
+const SbtGenerator: NextPage = () => {
+  const router = useRouter();
+  const { SbtAddress, network } = router.query;
+
+  const { account, address } = useAccount();
+  const [isMainnet, setIsMainnet] = useState<boolean>(true);
+  const [contractAddress, setContractAddress] = useState<string>("");
+  const [privateKey, setPrivateKey] = useState<BN>();
+  const [tokenId, setTokenId] = useState<string>("0");
+  const [passFailed, setPassFailed] = useState<boolean>(false);
+  const [password, setPassword] = useState<string>("");
+  const [sbtData, setSbtData] = useState<MetadataProps>();
+  const [callData, setCallData] = useState<CallDataProps[]>();
+
+  const { execute } = useStarknetExecute({
+    calls: callData as any,
+  });
+  const { contract } = useContract({
+    address: contractAddress,
+    abi: sbt_abi,
+  });
+  const { data, loading, error, refresh } = useStarknetCall({
+    contract,
+    method: "get_uri",
+    args: [1],
+    options: {
+      watch: false,
+    },
+  });
+
+  useEffect(() => {
+    if (network === "testnet") {
+      setIsMainnet(false);
+    }
+    setContractAddress(SbtAddress as string);
+  }, [SbtAddress, network]);
+
+  // Fetch info from contract
+  useEffect(() => {
+    if (!sbtData && data) {
+      let dataUri = "";
+      data[0].forEach((c: any) => {
+        dataUri += String.fromCharCode(c);
+      });
+      const metadataUri = dataUri.replace("ipfs://", "https://ipfs.io/ipfs/");
+      fetch(metadataUri)
+        .then((response) => response.json())
+        .then((metadata) => {
+          setSbtData(metadata);
+        });
+    }
+  });
+
+  useEffect(() => {
+    if (account && callData && callData.length > 0) execute();
+  }, [callData]);
+
+  function changeTokenId(value: string): void {
+    setTokenId(value);
+  }
+
+  function changePassword(value: string): void {
+    setPassword(value);
+  }
+
+  function updateCallData(): void {
+    const actualTokenId =
+      tokenId && tokenId != "0"
+        ? tokenId
+        : Math.floor(Math.random() * 9999999999);
+    const sbt_id = new BN(Math.floor(Math.random() * 9999999999));
+    const hashed = hash.pedersen([sbt_id, actualTokenId]);
+    const sbt_key = ec.genKeyPair();
+    const sbt_proof = ec.sign(sbt_key, hashed);
+    const whitelist_sig = ec.sign(ec.getKeyPair(privateKey as BN), hashed);
+    const calls =
+      tokenId === actualTokenId
+        ? []
+        : [
+            {
+              contractAddress: isMainnet
+                ? (process.env
+                    .NEXT_PUBLIC_STARKNETID_CONTRACT_MAINNET as string)
+                : (process.env
+                    .NEXT_PUBLIC_STARKNETID_CONTRACT_TESTNET as string),
+              entrypoint: "mint",
+              calldata: [actualTokenId],
+            },
+          ];
+    calls.push({
+      contractAddress: contractAddress,
+      entrypoint: "claim",
+      calldata: [
+        sbt_id.toString(),
+        actualTokenId,
+        ec.getStarkKey(sbt_key),
+        sbt_proof[0],
+        sbt_proof[1],
+        whitelist_sig[0],
+        whitelist_sig[1],
+      ],
+    });
+    calls.push({
+      contractAddress: isMainnet
+        ? (process.env.NEXT_PUBLIC_STARKNETID_CONTRACT_MAINNET as string)
+        : (process.env.NEXT_PUBLIC_STARKNETID_CONTRACT_TESTNET as string),
+      entrypoint: "equip",
+      calldata: [contractAddress, sbt_id.toString()],
+    });
+    setCallData(calls);
+  }
+
+  function handlePassword() {
+    const textAsBuffer = new TextEncoder().encode(password.toLowerCase());
+    (async () => {
+      const hashBuffer = await window.crypto.subtle.digest(
+        "SHA-256",
+        textAsBuffer
+      );
+      const privateKey = new BN(new Uint8Array(hashBuffer)).mod(
+        new BN(
+          "3618502788666131213697322783095070105526743751716087489154079457884512865583"
+        )
+      );
+      if (privateKey.clone().mod(new BN(5915587277)).toNumber() == 5122445791) {
+        setPrivateKey(privateKey);
+        setPassFailed(false);
+      } else {
+        setPassFailed(true);
+      }
+    })();
+  }
+
+  return (
+    <>
+      <main className={styles.main}>
+        <div className={[styles.card, !address ? styles.blur : ""].join(" ")}>
+          <img
+            className={styles.identityTokenImage}
+            src={
+              sbtData
+                ? sbtData?.image.replace("ipfs://", "https://ipfs.io/ipfs/")
+                : "/visuals/sbtIllustration.webp"
+            }
+            alt="SBT image"
+          />
+          <div className={styles.textSection}>
+            {privateKey ? (
+              <>
+                <h1 className={styles.title}>It's almost done</h1>
+                <div className={styles.identitySection}>
+                  <SelectIdentity
+                    tokenId={tokenId}
+                    changeTokenId={changeTokenId}
+                  />
+                  <Button onClick={updateCallData}>Mint my token</Button>
+                </div>
+              </>
+            ) : (
+              <>
+                <h1 className={styles.title}>
+                  {sbtData ? sbtData?.name : "Fetching SBT data..."}
+                </h1>
+                <p className={styles.text}>
+                  {sbtData
+                    ? sbtData?.desc
+                    : "With Account Abstraction, SBTs on Starknet are broken. It's the reason why the starknet.id team created the Identity Token, a new standard for SBTs on Starknet. Be part of this first experience on Starknet and mint your identity token for the hacker house now !"}{" "}
+                  <a
+                    href="https://github.com/starknet-id/identity_tokens#readme"
+                    className="underline cursor-pointer"
+                  >
+                    (Here is how it works)
+                  </a>
+                </p>
+                <div className={styles.password}>
+                  {account && sbtData ? (
+                    <>
+                      <TextField
+                        fullWidth
+                        type="password"
+                        label={
+                          passFailed
+                            ? "Try again it's not the valid password"
+                            : "Password"
+                        }
+                        placeholder="Password"
+                        variant="outlined"
+                        onChange={(e) => changePassword(e.target.value)}
+                        error={passFailed}
+                        required
+                      />
+                      <div className={styles.passwordBtn}>
+                        <Button onClick={() => handlePassword()}>Mint</Button>
+                      </div>
+                    </>
+                  ) : null}
+                </div>
+              </>
+            )}
+          </div>
+        </div>
+        {!address ? <Connect /> : <SbtNetworkSelector isMainnet={isMainnet} />}
+      </main>
+    </>
+  );
+};
+
+export default SbtGenerator;
